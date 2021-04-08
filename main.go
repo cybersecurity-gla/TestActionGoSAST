4af532b8c8ba11b30b5037a733e12d7c0a1aacd8
Bravo German <gebravo@andreani.com>
2021-04-08 16:49:07 -0300

    First commit

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	salud()
+}
+
+func saludo() {
+	fmt.Println("Hello, World!")
+}
---
+++ scanner.bat
@@ -0,0 +1,2 @@
+go run ../analizador/main.go
+sonar-scanner -Dsonar.login=65e8f767de06cb75878d3c26a70b3541c0c811ad
